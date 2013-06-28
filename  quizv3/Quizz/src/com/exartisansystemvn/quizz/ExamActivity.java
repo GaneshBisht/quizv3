3bb461de6916fbf57713be424cdace7a094eedb0
anhtuan.bk.55 <anhtuan.bk.55@gmail.com>
2013-06-28 22:24:37 +0000

    push adapter update some activities

---  quizv3/Quizz/src/com/exartisansystemvn/quizz/ExamActivity.java
+++  quizv3/Quizz/src/com/exartisansystemvn/quizz/ExamActivity.java
@@ -1,15 +1,31 @@
 package com.exartisansystemvn.quizz;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ExamActivity extends Activity {
+import com.exartisansystemvn.adapter.ExamQuizListAdapter;
+
+public class ExamActivity extends BaseActivity {
+	
+	private TextView tvSubject;
+	private TextView tvTime;
+	private ListView listQuiz;
+	private Button btnCheckMark;
+	private Button btnRetry;
+	private String subject;
+	private String time;
+	private OnClickListener btnClickListener;
+	private ExamQuizListAdapter examQuizListAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.exam);
 	}
 
 	@Override
@@ -17,6 +33,58 @@ public class ExamActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.exam, menu);
 		return true;
+	}
+
+	@Override
+	protected void displayActivity() {
+		setContentView(R.layout.exam);
+	}
+
+	@Override
+	protected void initViews() {
+		tvSubject = (TextView) findViewById(R.id.tv_subject_title_value);
+		tvSubject.setText(subject);
+		tvTime = (TextView) findViewById(R.id.tv_exam_time_title_value);
+		tvTime.setText(time);
+		listQuiz = (ListView) findViewById(R.id.listExamQizz);
+		listQuiz.setAdapter(examQuizListAdapter);
+		btnCheckMark = (Button) findViewById(R.id.btnCheckExamMark);
+		btnCheckMark.setOnClickListener(btnClickListener);
+		btnRetry = (Button) findViewById(R.id.btnRetryExam);
+		btnRetry.setOnClickListener(btnClickListener);
+	}
+
+	@Override
+	protected void initVariables() {
+		subject = " " + getIntent().getExtras().getString("subject");
+		time = " " + getIntent().getExtras().getString("time") + "'";
+		examQuizListAdapter = new ExamQuizListAdapter(this, examinationLibrary.get(subject.trim()));
+		btnClickListener = new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if (v==btnCheckMark) {
+					examQuizListAdapter.setAnsDisable(true);
+					examQuizListAdapter.notifyDataSetChanged();
+					int count = 0;
+					for (boolean e : examQuizListAdapter.getAnsChecks()) {
+						if (e == true)
+							count++;
+					}
+					Toast toast = Toast.makeText(getBaseContext(), "" + count, Toast.LENGTH_SHORT);
+					toast.show();
+				} else {
+					
+				}
+				
+			}
+		};
+	}
+
+	@Override
+	protected void initActions() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
---  quizv3/Quizz/src/com/exartisansystemvn/quizz/ExamHomeActivity.java
+++  quizv3/Quizz/src/com/exartisansystemvn/quizz/ExamHomeActivity.java
@@ -1,15 +1,28 @@
 package com.exartisansystemvn.quizz;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
-public class ExamHomeActivity extends Activity {
-
+public class ExamHomeActivity extends BaseActivity {
+	
+	private Spinner spinnerExamTime;
+	private Spinner spinnerExamSubject;
+	private Button btnStartExam;
+	private Button btnCancelExam;
+	private OnClickListener btnClickListener;
+	private ArrayAdapter<String> subjectAdapter;
+	private ArrayAdapter<Integer> timeAdapter;
+	//private OnItemSelectedListener spinnerlListener;
+	private Integer[] timeArray = {30, 45, 60};
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.exam_home);
 	}
 
 	@Override
@@ -17,6 +30,52 @@ public class ExamHomeActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.exam, menu);
 		return true;
+	}
+
+	@Override
+	protected void displayActivity() {
+		setContentView(R.layout.exam_home);
+		
+	}
+
+	@Override
+	protected void initViews() {
+		spinnerExamTime = (Spinner) findViewById(R.id.spinner_exam_time);
+		spinnerExamTime.setAdapter(timeAdapter);
+		spinnerExamSubject = (Spinner) findViewById(R.id.spinner_exam_subject);
+		spinnerExamSubject.setAdapter(subjectAdapter);
+		btnCancelExam = (Button) findViewById(R.id.btnCancelExam);
+		btnCancelExam.setOnClickListener(btnClickListener);
+		btnStartExam = (Button) findViewById(R.id.btnStartExam);
+		btnStartExam.setOnClickListener(btnClickListener);
+		
+	}
+
+	@Override
+	protected void initVariables() {
+		subjectAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,lstExaminationName);
+		subjectAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		timeAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, timeArray);
+		timeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        btnClickListener = new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if (v==btnStartExam){
+					Intent i = new Intent(getBaseContext(), ExamActivity.class);
+					i.putExtra("time", spinnerExamTime.getSelectedItem().toString());
+					i.putExtra("subject", spinnerExamSubject.getSelectedItem().toString());
+					startActivity(i);
+				}
+				
+			}
+		};
+	}
+
+	@Override
+	protected void initActions() {
+		
+		
 	}
 
 }
