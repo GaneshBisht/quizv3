54c0f9834cde1d47fffdc5b39fb525ad83e02b2c
slim.tbb@gmail.com <slim.tbb@gmail.com@b893a098-87f5-0fa7-bbb5-d6b5615488e1>
2013-07-02 06:22:25 +0000

    - Tách phần xử lý file thành riêng 1 class được đặt trong package util: WorkingWithDocumentExtensions - Sửa lại logic đọc file text

--- trunk/ quizv3/Quizz/src/com/exartisansystemvn/quizz/BaseActivity.java
+++ trunk/ quizv3/Quizz/src/com/exartisansystemvn/quizz/BaseActivity.java
@@ -1,23 +1,17 @@
 package com.exartisansystemvn.quizz;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileFilter;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Environment;
 import android.preference.PreferenceManager;
-import android.util.Log;
 
 import com.exartisansystemvn.bean.Quiz;
+import com.exartisansystemvn.util.WorkingWithDocumentExtensions;
 
 public abstract class BaseActivity extends Activity {
 	protected static boolean didStart = false;
@@ -70,94 +64,15 @@ public abstract class BaseActivity extends Activity {
 	private void doPreparationProcessOfApp() {
 		ArrayList<File> lstQuizFile = new ArrayList<File>();
 		ArrayList<Quiz> lstQuiz = new ArrayList<Quiz>();
-		lstQuizFile = scanForQizzFileFromSDCard("/Test");
+		WorkingWithDocumentExtensions docWorker = new WorkingWithDocumentExtensions();
+		lstQuizFile = docWorker.scanFilesInAFolderFromSDCard("test", ".txt");
 		for (File efile : lstQuizFile) {
 			String filename = efile.getName();
 			String examname = filename.substring(0, filename.indexOf("."));
 			lstExaminationName.add(examname);
-			lstQuiz = readContentOfQuizFile(efile);
+			lstQuiz = docWorker.readContentOfQuizFile(efile);
 			examinationLibrary.put(examname, lstQuiz);
 		}
-	}
-
-	public ArrayList<File> scanForQizzFileFromSDCard(String dir) {
-		ArrayList<File> alFile = new ArrayList<File>();
-		File folder = new File(Environment.getExternalStorageDirectory()
-				.getAbsolutePath() + dir);
-		// String[] listQuizz;
-		// ArrayList<String> listQuizz = new ArrayList<String>();
-		folder.mkdirs();
-		FileFilter filter = new FileFilter() {
-
-			@Override
-			public boolean accept(File pathname) {
-				return pathname.getAbsolutePath().matches(".*\\.txt");
-			}
-		};
-		// for (int i = 0; i < folder.listFiles(filter).length; i++)
-		// listQuizz.add(folder.listFiles(filter)[i].getName());
-		alFile.addAll(Arrays.asList(folder.listFiles(filter)));
-		return alFile;
-	}
-
-	/**
-	 * Read line by line of the Quiz File and handle each line by following
-	 * determind it is question or answer or the end of quiz. Then, the value
-	 * them are set into properties of the Quiz class. After all, we've got a
-	 * list of quizs.
-	 * 
-	 * @param strDir
-	 *            - Directory: String
-	 * @param strFileName
-	 *            - File name: String
-	 * @return List of quiz in the Quiz File
-	 */
-	public ArrayList<Quiz> readContentOfQuizFile(File aFile) {
-		ArrayList<Quiz> alQuiz = new ArrayList<Quiz>();
-		ArrayList<String> answers = new ArrayList<String>();
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(aFile));
-			String line;
-			int i = -1;
-			int correctAnswer = 0;
-			boolean isPriviousEmptyLine = false;
-			String question = null;
-			// reading a line is a loop. End loop if the text file has no more
-			// lines.
-			do {
-				line = br.readLine();
-				if (line != null) {
-					line = line.trim();
-					//Log.e("count" , " " + Integer.valueOf(line.charAt(0)));
-				}
-				if (line == null || line.equals("")) {
-					if (isPriviousEmptyLine||question == null)
-						continue;
-					alQuiz.add(new Quiz(question, answers, correctAnswer));
-					i = -1;
-					answers = new ArrayList<String>();
-					isPriviousEmptyLine = true;
-				} else if (i == -1) {
-					isPriviousEmptyLine = false;
-					question = line;
-					i++;
-				} else {
-					isPriviousEmptyLine = false;
-					if (line.startsWith("*")) {
-						answers.add(new String(line.substring(1)));
-						correctAnswer = i;
-					} else
-						answers.add(line);
-					i++;
-				}
-			} while (line != null);
-			br.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (NullPointerException e) {
-			Log.e("NullPointerException", "because of method: trim()");
-		}
-		return alQuiz;
 	}
 
 	private void getSettings() {
